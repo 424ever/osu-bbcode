f0c5ed48a934e14ff4d16eb19ccb422f8d4977ca
Jonas Brandstötter <jonas.brandstoetter@gmx.at>
2024-05-21 22:23:56 +0200

    Add .clang-format

--- src/main.c
+++ src/main.c
@@ -5,75 +5,84 @@
 
 void process(const char *ifname, const char *ofname);
 
-int main(int argc, char *argv[]) {
-  int opt;
-  int closein;
-  int ret;
-  int closeout;
-  const char *ofname;
-  FILE *ofile;
-  const char *ifname;
-  FILE *ifile;
+int main(int argc, char *argv[])
+{
+	int	    opt;
+	int	    ret;
+	const char *ofname;
+	FILE	   *ofile;
+	const char *ifname;
+	FILE	   *ifile;
 
-  ofname = NULL;
-  ret = EXIT_SUCCESS;
+	ofname = NULL;
+	ofile  = NULL;
+	ret    = EXIT_SUCCESS;
 
-  while ((opt = getopt(argc, argv, "o:")) != -1) {
-    switch (opt) {
-    case 'o':
-      ofname = optarg;
-      break;
-    case ':':
-      fprintf(stderr, "option '-%c' requires an operand\n", optopt);
-      ret = EXIT_FAILURE;
-      goto exit;
-    case '?':
-      fprintf(stderr, "unknown option '-%c'\n", optopt);
-      ret = EXIT_FAILURE;
-      goto exit;
-    }
-  }
+	while ((opt = getopt(argc, argv, "o:")) != -1)
+	{
+		switch (opt)
+		{
+		case 'o':
+			ofname = optarg;
+			break;
+		case ':':
+			fprintf(stderr, "option '-%c' requires an operand\n",
+				optopt);
+			ret = EXIT_FAILURE;
+			goto exit;
+		case '?':
+		default:
+			fprintf(stderr, "unknown option '-%c'\n", optopt);
+			ret = EXIT_FAILURE;
+			goto exit;
+		}
+	}
 
-  if (optind >= argc) {
-    closein = 0;
-    ifile = stdin;
-    ifname = "(stdin)";
-  } else {
-    closein = 1;
-    ifname = argv[optind];
-    ifile = fopen(ifname, "r");
-    if (ifile == NULL) {
-      perror(ifname);
-      goto close_exit;
-    }
-  }
+	if (optind >= argc)
+	{
+		ifile  = stdin;
+		ifname = "(stdin)";
+	}
+	else
+	{
+		ifname = argv[optind];
+		ifile  = fopen(ifname, "r");
+		if (ifile == NULL)
+		{
+			perror(ifname);
+			goto close_exit;
+		}
+	}
 
-  if (ofname == NULL) {
-    closeout = 0;
-    ofile = stdout;
-    ofname = "(stdout)";
-  } else {
-    closeout = 1;
-    ofile = fopen(ofname, "w");
-    if (ofile == NULL) {
-      perror(ofname);
-      goto close_exit;
-    }
-  }
+	if (ofname == NULL)
+	{
+		ofile  = stdout;
+		ofname = "(stdout)";
+	}
+	else
+	{
+		ofile = fopen(ofname, "w");
+		if (ofile == NULL)
+		{
+			perror(ofname);
+			goto close_exit;
+		}
+	}
 
-  process(ifname, ofname);
+	process(ifname, ofname);
 
 close_exit:
-  if (closein && ifile)
-    fclose(ifile);
+	if (ifile && ifile != stdin && fclose(ifile) == EOF)
+		perror("fclose(ifile)");
 
-  if (closeout && ofile)
-    fclose(ofile);
+	if (ofile && ofile != stdout && fclose(ifile) == EOF)
+		perror("fclose(ofile)");
 
 exit:
-  exit(ret);
+	exit(ret);
 }
 
-void process(const char *ifname, const char *ofname) {
-  fprintf(stdout, "will process '%s' -> '%s'\n", ifname, ofname);
+void process(const char *ifname, const char *ofname)
+{
+	fprintf(stdout, "will process '%s' -> '%s'\n", ifname, ofname);
 }
