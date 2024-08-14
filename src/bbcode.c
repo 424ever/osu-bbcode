7f53e17aa7203cda039eb782a83baf5e03e785ce
Jonas Brandstötter <jonas.brandstoetter@gmx.at>
2024-08-14 23:09:25 +0200

    move free impl to bbcode.c

--- src/bbcode.c
+++ src/bbcode.c
@@ -1,6 +1,9 @@
 #include "bbcode.h"
 #include "alloc.h"
 
+static void frag_free(struct bbcode_frag *);
+static void frag_list_free(struct bbcode_frag_list *);
+
 void frag_list_append(struct bbcode_frag_list *l, struct bbcode_frag *f)
 {
 	while (l->next != NULL)
@@ -9,4 +12,41 @@ void frag_list_append(struct bbcode_frag_list *l, struct bbcode_frag *f)
 	l	= l->next;
 	l->frag = f;
 	l->next = NULL;
+}
+
+static void frag_free(struct bbcode_frag *f)
+{
+	switch (f->type)
+	{
+	case TAG:
+		free((void *) f->tag.name);
+		frag_list_free((void *) f->tag.children);
+		break;
+	case TEXT:
+		free((void *) f->text.text);
+		break;
+	}
+	free(f);
+}
+
+static void frag_list_free(struct bbcode_frag_list *l)
+{
+	struct bbcode_frag_list *cur;
+	struct bbcode_frag_list *prev;
+
+	cur = l;
+	while (cur != NULL)
+	{
+		prev = cur;
+		cur  = cur->next;
+
+		frag_free(prev->frag);
+		free((void *) prev);
+	}
+}
+
+void bbcode_doc_free(struct bbcode_doc *doc)
+{
+	frag_list_free(doc->root_fragments);
+	free((void *) doc);
 }
